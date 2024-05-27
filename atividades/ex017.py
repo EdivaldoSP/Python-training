1029f544ec957a67e93ec6bdb41917e1865b1456
Edivaldo Sousa Pinheiro <161406590+EdivaldoSP@users.noreply.github.com>
2024-05-27 17:41:52 -0300

    Update ex017.py

--- atividades/ex017.py
+++ atividades/ex017.py
@@ -1,25 +1,39 @@
 import time
 import math
 # # PROGRAMA QUE CALCULA A HIPOTENUSA DE UM TRIÂNGULO RETÂNGULO
+
+# CALCULANDO SEM USAR A BIBLIOTECA MATH 👇
+
 print('{:=^30}'.format('| SEJA BEM VINDO (A)|'))
 
-oposto = int(input('--> Digite o comprimento do cateto oposto: '))
-adjacente = int(input('--> Digite o comprimento do cateto adjacente: '))
+oposto = float(input('--> Digite o comprimento do cateto oposto: '))
+adjacente = float(input('--> Digite o comprimento do cateto adjacente: '))
 
 # fórmula: c² = a² + b²
 
-hipotenusa1 = (oposto ** 2) + (adjacente ** 2)
-
-hipotenusa2 = math.isqrt(hipotenusa1)
+hipotenusa1 = (oposto ** 2 + adjacente ** 2) ** (1/2)
 
 print('Calculando, aguarde...')
 
 time.sleep(2)
 
-print('O comprimento da hipotenusa é {}.'.format(hipotenusa2))
+print('O comprimento da hipotenusa é {:.2f}.'.format(hipotenusa1))
 
 print('{:=^30}'.format('| OBRIGADO POR USAR |'))
 
-# teste = int(input('Digite numeoro: '))
+# CALCULANDO USANDO A BIBLIOTECA MATH
 
-# print('A raiz de {} é {}'.format(teste, math.sqrt(teste)))
+print('{:=^30}'.format('| SEJA BEM VINDO (A) |'))
+
+aposto= float(input('--> Digite o comprimento do cateto oposto: '))
+adjacente = float(input('--> Digite o comprimento do cateto adjacente: '))
+
+hipotenusa = math.hypot(aposto, adjacente)
+
+print('Calculando, aguarde...')
+
+time.sleep(2)
+
+print('O comprimento da hipotenusa é {:.2f}.'.format(hipotenusa))
+
+print('{:=^30}'.format('| OBRIGADO POR USAR |'))
