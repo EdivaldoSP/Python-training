dee77802d1bb910cda9e787738ca5e20cab4054a
Edivaldo Sousa Pinheiro <161406590+EdivaldoSP@users.noreply.github.com>
2024-05-27 18:19:43 -0300

    Update ex018.py

--- atividades/ex018.py
+++ atividades/ex018.py
@@ -5,16 +5,16 @@ import math
 
 print('{:=^30}'.format('| SEJA BEM VIDO (A) |'))
 
-angulo = int(input('--> Digite o ângulo: '))
+angulo = float(input('--> Digite o ângulo: '))
 
-seno = math.sin(angulo)
-cosseno = math.cos(angulo)
-tangente = math.tan(angulo)
+seno = math.sin(math.radians(angulo))
+cosseno = math.cos(math.radians(angulo))
+tangente = math.tan(math.radians(angulo))
 
 print('Calculando, aguarde...')
 
 time.sleep(2)
 
-print('Dado o ângulo {}, o seno é: {:.2f}, o cosseno é: {:.2f} e a tangente é: {:.2f}'.format(angulo, seno, cosseno, tangente))
+print('Dado o ângulo {}, o SENO é: {:.2f}, o COSSENO é: {:.2f} e a TANGENTE é: {:.2f}'.format(angulo, seno, cosseno, tangente))
 
 print('{:=^30}'.format('| OBRIGADO POR USAR |'))
