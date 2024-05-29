2124274557a63c2d5a8e454196749e301b285da1
Edivaldo Sousa Pinheiro <161406590+EdivaldoSP@users.noreply.github.com>
2024-05-29 12:13:43 -0300

    Update ex013.py

--- atividades/ex013.py
+++ atividades/ex013.py
@@ -16,21 +16,4 @@ print(f'O novo salário do funcionário com 15% de aumento é de: R$ {salario_li
 
 print('{:=^30}'.format('| OBRIGADO POR USAR |'))
 
-# Os arquivos enviados foram:
-
-# 1- Extrato Bolsa Familia: Elenir Costa de Sousa
-# Meses: Dezembro (2023), Janeiro (2024), Fevereiro (2024).
-# Fotos: 3.
-
-# 2- Extrato Picpay: Edivaldo Sousa Pinheiro
-# Meses: Dezembro (2023), Janeiro (2024), Fevereiro (2024).
-# Fotos: 52.
-
-# 3- Extrato Nubank: Ellen de Sousa Pinheiro
-# Meses: Dezembro (2023), Janeiro (2024), Fevereiro (2024).
-# Fotos: 21.
-
-# Resumo: Foi anexados os arquivos (fotos) da minha mãe, Elenir Costa de Sousa; do meu irmão, Edivaldo Sousa Pinheiro;
-# e os meus, Ellen de Sousa Pinheiro, dos meses: Dezembro (2023), Janeiro (2024), Fevereiro (2024).
-
 
