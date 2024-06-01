8c4ca993f7dbeb1c834f07222c5242a465a41456
Edivaldo Sousa Pinheiro <161406590+EdivaldoSP@users.noreply.github.com>
2024-05-31 21:48:43 -0300

    Create ex027.py

---
+++ atividades/ex027.py
@@ -0,0 +1,12 @@
+# PROGRAMA QUE LÊ O NOME COMPLETO E MOSTRA O PRIMEIRO E O ÚLTIMO NOME SEPARADO
+
+print('{:=^30}'.format('| SEJA BEM VINDO (A) |'))
+
+nome = str(input('--> Digite seu nome completo: '))
+
+dividir = nome.split()
+
+print('O primeiro nome é: {}'.format(dividir[0]))
+print('O segundo nome é: {}'.format(dividir[-1]))
+
+print('{:=^30}'.format('| OBRIGADO POR USAR |'))
