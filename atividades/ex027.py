395ed31c168c7f026b06530bf2ff4c227680c627
Edivaldo Sousa Pinheiro <161406590+EdivaldoSP@users.noreply.github.com>
2024-09-03 14:07:00 -0300

    Update ex027.py

--- atividades/ex027.py
+++ atividades/ex027.py
@@ -7,6 +7,6 @@ nome = str(input('--> Digite seu nome completo: '))
 dividir = nome.split()
 
 print('O primeiro nome é: {}'.format(dividir[0]))
-print('O segundo nome é: {}'.format(dividir[-1]))
+print('O último nome é: {}'.format(dividir[-1]))
 
 print('{:=^30}'.format('| OBRIGADO POR USAR |'))
