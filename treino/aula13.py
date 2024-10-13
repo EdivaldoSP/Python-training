29990376b81183703cd60f9bbf8819490f7f7e4f
Edivaldo Sousa Pinheiro <161406590+EdivaldoSP@users.noreply.github.com>
2024-10-13 16:52:19 -0300

    criei a pasta "treino" e coloquei dois arquivos

---
+++ treino/aula13.py
@@ -0,0 +1,6 @@
+n = int(input("Digite um número: "))
+
+for n in range(0, n+1):
+    print(n)
+print("FIM")
+
---
+++ treino/aula14.py
@@ -0,0 +1,7 @@
+contador = 0
+
+while contador < 10:
+    print(f"Número: {contador +1}")
+    contador += 1
+print(f"Número total: {contador}")
+
